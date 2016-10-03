53cec1d4a6c03e746ede3abaac1426666e2d712d
Christopher Syers <csyers@nd.edu>
2016-10-03 13:14:42 -0400

    tested and ammended makefiles

--- client/client.c
+++ client/client.c
@@ -1,4 +1,13 @@
-/* Test
- *
- *
- * /
+/* Name: Tim Chang (tchang2), Christopher Syers (csyers)
+ * Date: October 3, 2016
+ * CSE 30264: Computer Networks
+ * Programming Assignment 3: Prototype FTP Connection
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
