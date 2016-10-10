9978c8e881dfbddd52f9539a8e5278c95357959b
Christopher Syers <csyers@nd.edu>
2016-10-09 22:47:43 -0400

    basic framework and XIT

--- client/myftp.c
+++ client/myftp.c
@@ -20,37 +20,47 @@
 
 int main(int argc, char* argv[])
 {
+    // check usage
     if(argc != 3)
     {
         fprintf(stderr,"usage: myftp server_name port\n");
         exit(1);
     }
     
+    // initialize varaibles
     struct hostent *hp;
     struct sockaddr_in sin;
     char *host;
     char buf[MAX_LINE];
+    char file[MAX_LINE];
     int s;
     int len;
+
+    // get host name from the first command line argument
     host = argv[1];
     hp = gethostbyname(host);
+
+    // if hostname is not resolvable, print error and exit
     if(!hp)
     {
         fprintf(stderr,"myftp: unknown host: %s\n",host);
         exit(1);
     }
     
+    // setup the sockaddr_in
     bzero((char *)&sin,sizeof(sin));
     sin.sin_family = AF_INET;
     bcopy(hp->h_addr,(char *)&sin.sin_addr,hp->h_length);
     sin.sin_port = htons(atoi(argv[2]));
 
+    // open a socket, exit if there is an error
     if((s=socket(PF_INET,SOCK_STREAM,0))<0)
     {
         fprintf(stderr,"myftp: could not open socket\n");
         exit(1);
     }   
 
+    // attempt to connect to server, exit if there is an error
     if(connect(s,(struct sockaddr *)&sin,sizeof(sin))<0)
     {
         fprintf(stderr,"myftp: error in connect call\n");
@@ -58,24 +68,96 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
+    // prompt user for command
+    printf("Enter command (REQ, UPL, LIS, MKD, RMD, CHD, DEL, XIT): ");
+
+    // while user is still inputting commands
     while(fgets(buf,sizeof(buf),stdin))
     {
+        // set last char in buffer to null
         buf[MAX_LINE-1] = '\0';
-        if(!strncmp(buf,"Exit",4))
+        len = strlen(buf);
+
+        // strip the newline character from the buffer
+        if((len-1 > 0) && (buf[len-1] == '\n'))
         {
-            printf("Goodbye!\n");
+            buf[len-1] = '\0';
+        }
+
+        // case: REQ
+        if(strcmp(buf,"REQ") == 0)
+        {
+            // send REQ to server, print error and exit on failure
+            if(send(s,buf,len+1,0)==-1)
+            {
+                fprintf(stderr,"myftp: error in send\n");
+                close(s);
+                exit(1);
+            }
+            // prompt user for filename
+            printf("Enter file to retrieve: ");
+            if(fgets(file,sizeof(file),stdin)<0)
+            {
+                fprintf(stderr,"myftp: error in fgets\n");
+                close(s);
+                exit(1);
+            }
+        // case: UPL
+        }
+        else if (strcmp(buf,"UPL") == 0)
+        {
+
+        // case: LIS
+        }
+        else if (strcmp(buf,"LIS") == 0)
+        {
+
+        // case: MKD
+        }
+        else if (strcmp(buf,"MKD") == 0)
+        {
+
+        // case: RMD
+        }
+        else if (strcmp(buf,"RMD") == 0)
+        {
+
+        // case: CHD
+        }
+        else if (strcmp(buf,"CHD") == 0)
+        {
+
+        // case: DEL
+        }
+        else if (strcmp(buf,"DEL") == 0)
+        {
+
+        // case: XIT
+        }
+        else if (strcmp(buf,"XIT") == 0)
+        {
+            // send the command to the server, print error send fails
+            if(send(s,buf,len+1,0)==-1)
+            {
+                fprintf(stderr,"myftp: error in send\n");
+                close(s);
+                exit(1);
+            }
+            // end the loop, quit the client connection
             break;
+        // case: unknown command
         }
-        len = strlen(buf)+1;
-        if(send(s,buf,len,0)==-1)
+        else 
         {
-            fprintf(stderr,"myftp: error in send\n");
-            close(s);
-            exit(1);
+            // print error
+            printf("myftp: invalid command\n");
         }
+        // prompt user for next command
+        printf("Enter command (REQ, UPL, LIS, MKD, RMD, CHD, DEL, XIT): ");
     }
 
+    // close the socket and print exit message
     close(s);
-
+    printf("\nmyftp: session has been closed\n");
     return 0;
 }
--- server/myftpd.c
+++ server/myftpd.c
@@ -21,28 +21,34 @@
 
 int main(int argc, char* argv[])
 {
+    // check usage
     if(argc != 2)
     {
         fprintf(stderr,"usage: myftpd port\n");
         exit(1);
     }
+
+    // initialize variables
     struct sockaddr_in sin;
     char buf[MAX_LINE];
     int len;
     int s, new_s;
     int opt = 1;
 
+    //etup the sockaddr_in
     bzero((char *)&sin,sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(atoi(argv[1]));
 
+    // open a socket, exit if there is an error
     if((s=socket(PF_INET,SOCK_STREAM,0)) < 0)
     {
         fprintf(stderr,"myftpd: error in socket creation\n");
         exit(1);
     }
 
+    // allow socket to be reused after a closed connection, exit on error
     if((setsockopt(s,SOL_SOCKET,SO_REUSEADDR,(char *)&opt,sizeof(int)))<0)
     {
         fprintf(stderr,"myftpd: error in setsocketopt\n");
@@ -50,6 +56,7 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
+    // bind the socket to the port passed in on the command line
     if((bind(s,(struct sockaddr *)&sin,sizeof(sin)))<0)
     {
         fprintf(stderr,"myftpd: error in bind\n");
@@ -57,6 +64,7 @@ int main(int argc, char* argv[])
         exit(1);
     }   
 
+    // begin listening for client connections on the socket
     if((listen(s,MAX_PENDING))<0)
     {
         fprintf(stderr,"myftpd: error in listen\n");
@@ -64,34 +72,65 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    printf("Welcome to the first TCP server!\n");
-
+    // enter infinte loop waiting for connections
     while(1){
+        // accept a client connection on a new socket (new_s), exit on error
         if((new_s=accept(s,(struct sockaddr *)&sin,(socklen_t *)&len))<-0)
         {
             fprintf(stderr,"myftpd: error in accept\n");
             close(s);
             exit(1);
         }
-        
+        // infinte loop while in connection with client
         while(1)
         {
+            // receive the command from the client, exit on error
             if((len=recv(new_s,buf,sizeof(buf),0))==-1)
             {
                 fprintf(stderr,"myftpd: server received error\n");
+                close(new_s);
                 close(s);
                 exit(1);
             }
-            if(len==0)
+            // case: REQ
+            if(strcmp(buf,"REQ") == 0)
             {
+            }
+            // case: UPL
+            else if(strcmp(buf,"UPL") == 0)
+            {
+            }
+            // case: LIS
+            else if(strcmp(buf,"LIS") == 0)
+            {
+            }
+            // case: MKD
+            else if(strcmp(buf,"MKD") == 0)
+            {
+            }
+            // case: RMD
+            else if(strcmp(buf,"RMD") == 0)
+            {
+            }
+            // case: CHD
+            else if(strcmp(buf,"CHD") == 0)
+            {
+            }
+            // case: DEL
+            else if(strcmp(buf,"DEL") == 0)
+            {
+            }
+            // case: XIT or termination of client
+            if(len==0 || strcmp(buf,"XIT") == 0)
+            {
+                // enter accepting state
                 break;
             }
-            printf("%s",buf);
-
         }
         close(new_s);
     }
 
+    // close socket and end program
     close(s);
     return 0;
 }
